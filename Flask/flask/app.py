0bcad32b28a5952bd0dc20ad0cbc18b4d5f2c850
AyushM03 <aayushmeshram9168@gmail.com>
2026-09-13 13:54:09 +0530

    learning python flask skeleton

---
+++ Flask/flask/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+
+###WSGI application
+app=Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return "welcome to home page"
+
+@app.route('/about')
+def about():
+    return "welcome to about page"
+
+@app.route('/contact')
+def contact():
+    return "welcome to contact page"
+#entry point of the application
+if __name__ == '__main__':
+    app.run(debug=True)    
+    '''just because of debug=True,
+      the server will reload itself on code changes and show 
+      a debugger in case an exception happens.'''
